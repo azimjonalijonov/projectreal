74047afb6394200b128c9f9663622b0cdd3aaccd
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-12-15 20:38:16 +0500

    Initial commit

---
+++ src/main/java/myfirstpro/projectreal/ProjectrealApplication.java
@@ -0,0 +1,13 @@
+package myfirstpro.projectreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectrealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myfirstpro/projectreal/ProjectrealApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirstpro.projectreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
